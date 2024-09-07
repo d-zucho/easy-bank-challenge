a7bb1c940fa4870a21fae3db6227a719270c5d0c
D_Zucho <danny.nico.dev@outlook.com>
2024-09-07 16:57:06 -0700

    add hero section & responsive styles

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,20 +10,32 @@ type Props = {}
 const Hero = (props: Props) => {
   return (
     <section className='relative z-0 overflow-y-clip'>
+      <MobileHeroPattern className='absolute -z-10 -top-28 right-0 w-full md:w-auto md:overflow-visible ' />
+
       <MaxWidthWrapper className='w-full'>
-        <div className='grid md:grid-cols-2 '>
+        <div className='grid md:grid-cols-2  items-center'>
           {/* start images */}
-          <div className='w-full'>
-            <MobileHeroPattern className='w-full absolute -top-10 inset-0 -z-10 object-cover' />
+          <div className='w-full md:order-2'>
             <Image
               src={HeroImage}
               alt='hero'
               aria-hidden='true'
-              className='-mt-44'
+              width={500}
+              height={500}
+              className='-mt-40 mx-auto md:hidden'
+            />
+            <Image
+              src={HeroImage}
+              alt='hero'
+              // width={800}
+              // height={800}
+              aria-hidden='true'
+              className='-mt-24 -mr-10 mx-auto hidden md:block'
             />
           </div>
+
           {/* copy */}
-          <div className='flex flex-col gap-5 items-center lg:items-start'>
+          <div className='flex flex-col gap-5 items-center lg:items-start md:order-1'>
             <div className='text-center lg:text-left space-y-4'>
               <h1 className='text-4xl md:text-[46px] lg:text-[52px] text-primary-dark_blue'>
                 Next generation digital banking
@@ -34,7 +46,7 @@ const Hero = (props: Props) => {
                 much more.
               </p>
             </div>
-            <Button className='bg-my-gradient hover:scale-105 transition-all rounded-full px-8 text-sm font-semibold'>
+            <Button className='bg-my-gradient hover:scale-105 hover:shadow-xl transition-all rounded-full px-8 text-sm font-semibold'>
               Request Invite
             </Button>
           </div>
